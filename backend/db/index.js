62472a2246639f25c825533972facf3589705c79
Abhilash <pariharabhilash22@gmail.com>
2023-03-01 22:17:30 +0530

    migrate mongo uri to env

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const init = async () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/task-management", {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error: "));
   db.once("open", () => {
